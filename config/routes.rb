f05e6c9551fe032c4f2cf8f3b5ee42ad44a191ff
atok624 <84607674+atok624@users.noreply.github.com>
2023-07-06 10:53:02 +0000

    Update the route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  resources :categories do
+    resources :payments
+  end
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: "home#index"
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_scope :user do
+    get '/logout', to: 'devise/sessions#destroy'
+  end
+  root to: 'users#index'
 end
