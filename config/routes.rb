45ffc2d942b2083e105383c61c998de2c8c69f60
atok624 <84607674+atok624@users.noreply.github.com>
2023-07-08 01:02:59 +0000

    Update the route.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  resources :categories do
-    resources :payments
+  resources :categories, except: [:update] do
+    resources :payments, except: [:update]
   end
+
   devise_for :users
   devise_scope :user do
     get '/logout', to: 'devise/sessions#destroy'
   end
+
   root to: 'users#index'
 end
